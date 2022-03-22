813f8170c89b4db76f03df5fd5e9c5cccdc552c0
Mike Turner <mwtvy4@mst.edu>
2022-03-22 12:03:03 -0700

    pushing log before pruning

--- ServerSide/cloverlogger.py
+++ ServerSide/cloverlogger.py
@@ -11,8 +11,12 @@ with serial.Serial('/dev/ttyUSB0', timeout = 10, baudrate=115200) as ser:  # ope
     ser.write(b's')#send command to output data
     encodedData = ser.readline()
     greenhouseData = encodedData.decode() #translate bytes to string
-    #z = y[:-2] #removes last two characters, in this case, /r/n
-    print(y)
+    greenhouseData = greenhouseData[:-2] #removes last 3 characters, in this case, "/r/n and first "
+    print(greenhouseData)
+    greenhouseDataList = greenhouseData.split(',')
+    temp = greenhouseDataList[0]
+    humi = greenhouseDataList[1]
+    res = greenhouseDataList[2]
     ser.close()
 
 timestamp = time.time()
@@ -20,17 +24,21 @@ timestamp = time.time()
 try:
     with open('cloverLog.csv', 'a', newline='')as csvfile:
         append2file = csv.writer(csvfile, delimiter=',')
-        append2file.writerow([timestamp,greenhouseData])
+        append2file.writerow([timestamp,res,temp,humi])
 except Exception as e:
     print(e)
 
 x = []
-y = []
+resData = []
+humiData = []
+tempData = []
 with open('cloverLog.csv', newline='') as csvfile:
     filereader = csv.reader(csvfile, delimiter=',')
     for row in filereader:
         x.append(datetime.datetime.fromtimestamp(float(row[0])))
-        y.append(int(row[1]))
+        resData.append(int(row[1]))
+        tempData.append(float(row[2]))
+        humiData.append(float(row[3]))
 
     plt.xticks( rotation=25 )
     plt.ylabel('Ohms')
@@ -39,7 +47,32 @@ with open('cloverLog.csv', newline='') as csvfile:
     xfmt = md.DateFormatter('%m-%d %H:%M')
     ax=plt.gca()
     ax.xaxis.set_major_formatter(xfmt)
-    plt.title('Clover sand conductivity')
-    plt.plot(x[-1400:],y[-1400:],'g')
+    plt.title('Soil Resistance')
+    plt.plot(x[-1400:],resData[-1400:],'g')
     plt.savefig('cloverPlot.png')
+    plt.clf()
+
+    plt.xticks( rotation=25 )
+    plt.ylabel('%RH')
+#    plt.style.use('dark_background')
+    plt.subplots_adjust(bottom=0.2,left=0.2)
+    xfmt = md.DateFormatter('%m-%d %H:%M')
+    ax=plt.gca()
+    ax.xaxis.set_major_formatter(xfmt)
+    plt.title('Humidity')
+    plt.plot(x[-1400:],humiData[-1400:],'g')
+    plt.savefig('humiPlot.png')
+    plt.clf()
+
+    plt.xticks( rotation=25 )
+    plt.ylabel('*C')
+#    plt.style.use('dark_background')
+    plt.subplots_adjust(bottom=0.2,left=0.2)
+    xfmt = md.DateFormatter('%m-%d %H:%M')
+    ax=plt.gca()
+    ax.xaxis.set_major_formatter(xfmt)
+    plt.title('Temperature')
+    plt.plot(x[-1400:],tempData[-1400:],'g')
+    plt.savefig('tempPlot.png')
+
 
