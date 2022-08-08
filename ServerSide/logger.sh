73778ed7bde6cd5d25f87c0ea2fd12fa6eb2bb8e
Mike Turner <mwtvy4@mst.edu>
2022-08-07 17:53:13 -0700

    updated scripts

--- ServerSide/logger.sh
+++ ServerSide/logger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/git/Hydreader/ServerSide
-python3 ./cloverlogger.py
 python3 ./webcamcap.py
+bash ./timelapse.sh
--- ServerSide/webcamcap.py
+++ ServerSide/webcamcap.py
@@ -3,7 +3,7 @@ import cv2
 import time
 import datetime
 
-cam = VideoCapture(0)
+cam = cv2.VideoCapture(-1)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 cam.set(cv2.CAP_PROP_AUTOFOCUS, 0)
@@ -12,8 +12,8 @@ s, img = cam.read()
 if s:    # frame captured without any errors
 #    namedWindow("cam-test")
 #    imshow("cam-test",img)
-    waitKey(0)
+    cv2.waitKey(0)
 #    destroyWindow("cam-test")
     cropped = img[:,450:1720]
     rotated = cv2.rotate(cropped,cv2.ROTATE_90_CLOCKWISE)
-    imwrite("livecam.jpg",rotated) #save image
+    cv2.imwrite("livecam.jpg",rotated) #save image
